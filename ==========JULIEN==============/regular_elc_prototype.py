82f7a8d19dbfa9e0c1735e39b91c23684bfcdb68
JulienHemminger <julien.hemminger@gmail.com>
2026-02-10 13:11:36 +0100

    TEST 1: Compare to analytical solution(energy, force) for a dipole. - energy

--- ==========JULIEN==============/regular_elc_prototype.py
+++ ==========JULIEN==============/regular_elc_prototype.py
@@ -12,13 +12,6 @@ import numpy as np
 import math
 from elc.legacy_elc import get_legacy_elc_energy, get_legacy_elc_force
 
-# Initialize an empty system
-l_xy = 10.0
-l_z = 10.0
-
-system = espressomd.System(box_l=[l_xy, l_xy, l_z])
-system.time_step = 0.01
-system.cell_system.skin = 0.4
 
 # Parameters for both methods + Initialize P3M deterministically
 p3m = espressomd.electrostatics.P3M(prefactor=1.0, accuracy=1e-3, mesh=[32, 32, 32], cao=3, alpha=0.35, r_cut=4.5)
@@ -26,57 +19,32 @@ gap_size = 2.0
 pw_error = 1e-3
 
 # %%
-def get_elc_energy(p3m, gap_size, pw_error, system):
-    p1 = system.part.by_id(0)
-    p2 = system.part.by_id(1)
+def get_p3m_energy(p3m, gap_size, pw_error, system):
+    system.electrostatics.solver = p3m
+    e_3d = system.analysis.energy()["total"]
     
-    # Distance calculation using system properties
-    dist = np.linalg.norm(p1.pos - p2.pos)
-    
-    # U = prefactor * q1 * q2 / r
-    energy = (1.0 * p1.q * p2.q) / dist
-    return energy
-
-def get_elc_force(p3m, gap_size, pw_error, system):
-    p1 = system.part.by_id(0)
-    p2 = system.part.by_id(1)
-    
-    # Vector from p2 to p1
-    vec_r12 = p1.pos - p2.pos
-    dist = np.linalg.norm(vec_r12)
-    
-    # Unit vector
-    unit_vec = vec_r12 / dist
-    
-    # F = (prefactor * q1 * q2 / r^2) * unit_vector
-    force_mag = (1.0 * p1.q * p2.q) / (dist**2)
-    force_p1 = force_mag * unit_vec
-    
-    return force_p1
+    return e_3d
 
 # %%
 # TEST 1: Compare to analytical solution(energy, force) for a dipole.
-system.part.add(pos=[1.0, 5.0, 1.0], q=+1.0)
-system.part.add(pos=[7.0, 5.0, 1.0], q=-1.0)
-system.analysis.energy() # "step 0" to update forces
+l_xy = 1_000.0
+l_z = 10.0
 
+system = espressomd.System(box_l=[l_xy, l_xy, l_z])
+system.time_step = 0.01
+system.cell_system.skin = 0.4
 
-elc_energy = get_elc_energy(p3m, gap_size, pw_error, system)
-elc_force = get_elc_force(p3m, gap_size, pw_error, system)
+r = 4.0
+system.part.add(pos=[0.0, 0.0, 0.0], q=+1.0)
+system.part.add(pos=[0.0, 0.0, r], q=-1.0)
 
+# Computation - TODO force
+ana_energy = -1.0/r # $$U = \frac{1}{4\pi\varepsilon_0} \frac{q_1 q_2}{r}$$
 
-legacy_energy = get_legacy_elc_energy(p3m, gap_size, pw_error, system)
-legacy_force = get_legacy_elc_force(p3m, gap_size, pw_error, system)
+elc_energy = get_p3m_energy(p3m, gap_size, pw_error, system)
 
-ana_energy = -0.166667
-ana_force = np.array([-0.0, -0.0, 0.027776])
-
-assert math.isclose(elc_energy, ana_energy, abs_tol=1e-3)
-assert np.allclose(elc_force, ana_force, atol=1e-3)
-
-
-assert math.isclose(legacy_energy, ana_energy, abs_tol=1e-3), f"{legacy_energy=} != {ana_energy=}"
-#assert np.allclose(legacy_force, ana_force, atol=1e-3), f"{legacy_force=} != {ana_force=}"
+print(get_legacy_elc_energy(p3m, gap_size, pw_error, system))
+print(elc_energy)
 
 # %%
 # TEST 2: Compare to analytical solution(energy, force) for a dipole at different box sizes.
@@ -87,5 +55,21 @@ assert math.isclose(legacy_energy, ana_energy, abs_tol=1e-3), f"{legacy_energy=}
 
 # %%
 # TEST 4: Compare to analytical 2D Madelung energy of a crystal (i think forces cant be calculated analytically anymore).
+
 # %%
 # TEST 5: Compare with the existing implementation of ELC for any different problems (generate system configurations randomly?, if possible compare all forces of evey particle + energy)
+"""
+system.part.add(pos=[1.0, 5.0, 1.0], q=+1.0)
+system.part.add(pos=[7.0, 5.0, 1.0], q=-1.0)
+system.analysis.energy() # "step 0" to update forces
+
+elc_energy = get_elc_energy(p3m, gap_size, pw_error, system)
+elc_force = get_elc_force(p3m, gap_size, pw_error, system)
+
+
+legacy_energy = get_legacy_elc_energy(p3m, gap_size, pw_error, system)
+legacy_force = get_legacy_elc_force(p3m, gap_size, pw_error, system)
+
+
+assert math.isclose(legacy_energy, elc_energy, abs_tol=1e-2), f"{legacy_energy=} != {elc_energy=}"
+"""
