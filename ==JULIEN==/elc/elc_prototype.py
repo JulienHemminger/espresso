2d311af7909b523aa4fa4e6c8ef6d8d84126d4a9
JulienHemminger <julien.hemminger@gmail.com>
2026-02-22 15:43:14 +0100

    notes

--- ==JULIEN==/elc/elc_prototype.py
+++ ==JULIEN==/elc/elc_prototype.py
@@ -27,11 +27,13 @@ from elc.src.get_legacy_elc import get_legacy_elc_energy
 #dipole_variants_test()
 # %%
 # TEST 4: particle_count=3-10, non-neutral systems, varying charges q_i
-
-"""
+NO = []
+f"""
 
 * improve my elc
-    * maybe "decompose" it further. reward-estim: more tests, more problems/code/parts/errors
+    * maybe give gemini elc.cpp as info 
+
+    * maybe "decompose" it further: {NO} more tests, more problems/code/parts/errors
         * TEST4: "q_i is not always +-1"
         * TEST5: "random particle count (neutral system)"
         * TEST6: "random particle count (non-neutral system)
