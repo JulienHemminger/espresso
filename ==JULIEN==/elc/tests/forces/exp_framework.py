82a8c7cec3f06d3d5e1127e4e51a4e8b5232153f
JulienHemminger <julien.hemminger@gmail.com>
2026-03-05 14:38:28 +0100

    refac

--- ==JULIEN==/elc/tests/forces/exp_framework.py
+++ ==JULIEN==/elc/tests/forces/exp_framework.py
@@ -1,4 +1,4 @@
-def run_test(lx, ly, lz, gap_size, pw_error, charges=[]):
+def run_test(lx, ly, lz, gap_size, pw_error=1e-6, tolerance=1e-6, charges=[]):
     
     particle_count = len(charges)
     # generate positions
