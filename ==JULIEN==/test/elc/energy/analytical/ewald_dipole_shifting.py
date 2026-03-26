84a08e10e283345f11c261fd0abd735ed169ba7e
JulienHemminger <julien.hemminger@gmail.com>
2026-03-26 10:30:03 +0100

    refac

--- ==JULIEN==/test/elc/energy/analytical/ewald_dipole_shifting.py
+++ ==JULIEN==/test/elc/energy/analytical/ewald_dipole_shifting.py
@@ -4,43 +4,45 @@ import matplotlib.pyplot as plt
 import numpy as np
 from elc.energy.analytical_elc_energy import get_ewald_energy_2d
 
-l_xy = 10.0
-l_z = 3.0
-gap_size = 1.0
-system = espressomd.System(box_l=[l_xy, l_xy, l_z])
-system.time_step = 0.01
-system.cell_system.skin = 0.4
+def run(lx, ly, lz, gap_size, charges, positions, z_pos_count):
+    system = espressomd.System(box_l=[lx, ly, lz])
+    system.time_step = 0.01
+    system.cell_system.skin = 0.4
 
-z_range = np.linspace(0, l_z - gap_size - 1e-3, num=10)
-n_int = 100
-ewald_energies = []
+    z_range = np.linspace(0, lz - gap_size - 1e-3, num=z_pos_count)
+    ewald_energies = []
 
-for z in z_range:
-    system.part.clear()
+    for z in z_range:
+        system.part.clear()
+        for  i in range(min(len(charges), len(positions))):
+            pos = positions[i]
+            pos[2] = z
+            system.part.add(pos=pos, q=charges[i])
+        
 
-    system.part.add(pos=[3.0, 5.0, z], q=+2.0)
-    system.part.add(pos=[7.0, 5.0, z], q=-1.0)
-
-    e_ewald = get_ewald_energy_2d(system, n_int)
-    ewald_energies.append(e_ewald)
+        e_ewald = get_ewald_energy_2d(system)
+        ewald_energies.append(e_ewald)
 
 
-plt.figure(figsize=(10, 6))
+    plt.figure(figsize=(10, 6))
 
-plt.plot(
-    z_range,
-    ewald_energies,
-    "-",
-    color="tab:red",
-    linewidth=2,
-    label="Ewald 2D Energy ($n_{max}=100$)",
-)
+    plt.plot(
+        z_range,
+        ewald_energies,
+        "-",
+        color="tab:red",
+        linewidth=2,
+        label="Ewald 2D Energy ($n_{max}=100$)",
+    )
 
-plt.xlabel("Dipole Height ($z$)")
-plt.ylabel("Interaction Energy")
-plt.title("2D Ewald Energy vs. Vertical Position (z)")
-plt.legend()
-plt.grid(True, linestyle="--", alpha=0.6)
+    plt.xlabel("Dipole Height ($z$)")
+    plt.ylabel("Interaction Energy")
+    plt.title("2D Ewald Energy vs. Vertical Position (z)")
+    plt.legend()
+    plt.grid(True, linestyle="--", alpha=0.6)
 
-plt.tight_layout()
-plt.show()
+    plt.tight_layout()
+    plt.show()
+
+
+run(10, 10, 3, 1, np.array([+2, -1]), np.array([[3.0, 5.0, 0], [7.0, 5.0, 0]]), 10)
