daa77818397a0aa642bd7b556e31b39995678411
JulienHemminger <julien.hemminger@gmail.com>
2026-04-10 08:45:10 +0200

    refac

--- ==JULIEN==/test/elcic/elcic_method/b1_TEST_dual_plates_metallic_neutral.py
+++ ==JULIEN==/test/elcic/elcic_method/b1_TEST_dual_plates_metallic_neutral.py
@@ -9,22 +9,26 @@ system = espressomd.System(box_l=[1, 1, 1])
 system.time_step = 0.01
 
 params = {
-    "lx": 120.0,
-    "ly": 80.0,
-    "lz": 20.0,
-    "gap_size": 15.0,
+    "lx": 12.0,
+    "ly": 8.0,
+    "lz": 11.0,
+    "gap_size": 7.0,
     "prefactor": 1.0,
     "delta_mid_top": -1.0,
     "delta_mid_bot": -1.0, # for metallic it must: Δ = −1
     "charges": [+1, -1],
     'pw_error': 1e-8,
-    "positions": [np.array([2, 5, 3]), np.array([8, 3, 1])],
+    "positions": [np.array([1, 2, 3]), np.array([4, 5, 1])],
     "title": "Dual Plates, Both Metallic, Neutral"
-}# ANA: Iteration 10: energy(k_val=1024, n_val=100) = -0.35079175141122165
+}
 
 #params["positions"] = [np.array([2, 5, 0.01]), np.array([8, 3, 0.02])]
 #run_elcic_energy_accuracy_convergence(system, params)
 
 
-run_elcic_dipole_shifting(system, z_pos_count=4, params=params)
+run_elcic_dipole_shifting(system, z_pos_count=16, params=params)
 
+"""
+Theres a small error (1e-2?) bit idk yet where it comes from. Maybe i can "hide" it
+
+"""
