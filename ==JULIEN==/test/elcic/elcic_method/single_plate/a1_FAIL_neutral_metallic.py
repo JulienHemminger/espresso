8d1eb5c047c2096a5dcc9e263beff0cd8d0c14e2
JulienHemminger <julien.hemminger@gmail.com>
2026-04-27 12:44:37 +0200

    plot to show error correlates with p1.z

--- ==JULIEN==/test/elcic/elcic_method/single_plate/a1_FAIL_neutral_metallic.py
+++ ==JULIEN==/test/elcic/elcic_method/single_plate/a1_FAIL_neutral_metallic.py
@@ -6,46 +6,60 @@ from test.elcic.ana_method.param_sweep_accuracy_convergence import param_sweep_a
 system = espressomd.System(box_l=[1, 1, 1])
 system.time_step = 0.01
 
+import numpy as np
 
-params_count = 3
+# Configuration
+steps_per_coord = 3
+start_pos = [np.array([1.0, 1.0, 1.0]), np.array([2.0, 2.0, 2.0])]
+end_pos = [np.array([8.1454445, 34.44014336, 0.23335833]), 
+           np.array([11.20275155, 23.29430907, 0.7380634])]
+
 params_sets = []
 
-for i in range(params_count):
-  
-    params = {
-        "lx": 22.553239340502152,
-        "ly": 41.839080168737745,
-        "lz": 35.51793215353896,
-        "gap_size": 32.234687817568656,
-        "prefactor": 1.0,
-        "delta_mid_top": 0.0,
-        "delta_mid_bot": -1.0,
-        "pw_error": 1e-8,
-        "charges": [+1, -1],
-    }
+# Total steps = (Number of Particles * XYZ) * steps_per_coord
+# We'll iterate through each particle (p) and each axis (a)
+current_positions = [p.copy() for p in start_pos]
 
-    params["positions"] = [
-        np.array([
-            np.linspace(1.0, 8.1454445, params_count)[i],
-            np.linspace(1.0, 34.44014336, params_count)[i]  ,
-            np.linspace(1.0, 0.23335833, params_count)[i]  
-          
-        ]),
-        np.array([
-            np.linspace(2.0, 11.20275155, params_count)[i],
-            np.linspace(2.0, 23.29430907, params_count)[i]  ,
-            np.linspace(2.0, 0.7380634, params_count)[i]  
-          
-        ])
-    ]
+for p_idx in range(2):      # Particle 0 then Particle 1
+    for axis in range(3):   # X, then Y, then Z
+        
+        # Create the interpolation for this specific coordinate
+        start_val = start_pos[p_idx][axis]
+        end_val = end_pos[p_idx][axis]
+        
+        # Generate the intermediate values (excluding the very first start point 
+        # to avoid duplicates from the previous coordinate's end)
+        lerp_values = np.linspace(start_val, end_val, steps_per_coord + 1)[1:]
 
-    params_sets.append(params)
+        for val in lerp_values:
+            params = {
+                "lx": 22.553239340502152,
+                "ly": 41.839080168737745,
+                "lz": 35.51793215353896,
+                "gap_size": 32.234687817568656,
+                "prefactor": 1.0,
+                "delta_mid_top": 0.0,
+                "delta_mid_bot": -1.0,
+                "pw_error": 1e-8,
+                "charges": [+1, -1],
+            }
+            
+            # Update only the specific coordinate we are currently lerping
+            current_positions[p_idx][axis] = val
+            
+            # Store a deep copy of the positions state
+            params["positions"] = [p.copy() for p in current_positions]
+            params_sets.append(params)
+
+# Note: The very first state (all starts) isn't in the loop. 
+# You might want to prepend it manually:
+# params_sets.insert(0, initial_params_dict)
 
 param_sweep_accuracy_convergence(system, params_sets, accuracies = [10**-i for i in range(1, 11)])
 
 """
 ===params that cause errors===
-* positions, (especially p1.z, p2.z)
+* p1.z
 
 
 increasing gap_size increases error
