16ca8b83f9cf37bc8bdde6853479e5b9bf107e13
JulienHemminger <julien.hemminger@gmail.com>
2026-05-18 12:14:31 +0200

    refac set upü dual plate, non metallic, neutral

---
+++ ==JULIEN==/src/elcic/energy/single_plate/non_neutral/metallic/plot.py
@@ -0,0 +1,50 @@
+import copy
+
+import espressomd
+import numpy as np
+from elcic.energy.single_plate.neutral.metallic.custom import get_elcic_energy
+from elcic.energy.single_plate.neutral.metallic.analytical import (
+    analytical_single_plate_2d_ewald_elcic_energy,
+)
+from elcic.energy.shared.param_lerp_plot_2d import run_lerp_plot
+
+system = espressomd.System(box_l=[50, 50, 50])
+system.time_step = 0.01
+system.cell_system.skin = 0.4 # NEED to fix "tuning failed: number of cells 6 is smaller than minimum 8"
+
+def get_legacy_energy(system, params):
+    return 0
+
+start_params = {
+    "lx": 50.0,
+    "ly": 50.0,
+    "gap_size": 20.0,
+    "prefactor": 1.0,
+    "delta_mid_top": 0.0,
+    "delta_mid_bot": -1.0,
+    "charges": [+1.0, -1.0, -1.0],
+    "positions": [np.array([1, 2, 3]), np.array([4, 5, 6]), np.array([3, 1, 5])],
+    "pw_error": 1e-8,
+}
+start_params["lz"] = start_params["gap_size"] + 40
+
+
+#end_params = copy.deepcopy(start_params)
+#end_params["ly"] = 10
+
+
+end_params = {
+    "lx": 10.0,
+    "ly": 10.0,
+    "gap_size": 10.0,
+    "prefactor": 1.0,
+    "delta_mid_top": 0.0,
+    "delta_mid_bot": -1.0,
+    "charges": [+1.0, -1.0, -1.0],
+    "positions": [np.array([6, 5, 6]), np.array([3, 2, 1]), np.array([4, 2, 1])],
+    "pw_error": 1e-8,
+}
+end_params["lz"] = end_params["gap_size"] + 10
+
+
+run_lerp_plot(system, start_params, end_params, analytical_single_plate_2d_ewald_elcic_energy, get_legacy_energy, get_elcic_energy, steps=10)
