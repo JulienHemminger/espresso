d51e9d83decce5fe9f34cf92829aa9838bcf8ba6
JulienHemminger <julien.hemminger@gmail.com>
2026-05-18 11:36:47 +0200

    legacy elc added case for args["const_pot"] = True

--- ==JULIEN==/src/elc/energy/legacy_elc_energy.py
+++ ==JULIEN==/src/elc/energy/legacy_elc_energy.py
@@ -41,10 +41,12 @@ def get_legacy_energy(system, params_dict, timeout_duration_sec=30, timeout_retu
             args["delta_mid_top"] = delta_mid_top
         if delta_mid_bot is not None:
             args["delta_mid_bot"] = delta_mid_bot
+
         if delta_mid_top == -1 and delta_mid_bot == -1:
             args["const_pot"] = True
-
-        # This is usually where the 'tuning' hang happens
+        if delta_mid_top == +1 and delta_mid_bot == +1:
+            args["const_pot"] = True
+        
         elc_legacy = espressomd.electrostatics.ELC(**args)
 
         system.electrostatics.solver = elc_legacy
