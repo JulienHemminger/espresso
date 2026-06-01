2b0b3eac56b23f0323920fe656f2c30f8c84431f
JulienHemminger <julien.hemminger@gmail.com>
2026-06-01 20:31:18 +0200

    refac elc.cpp, rm sanity checks

--- ==JULIEN==/src/elcic/TEMP_elc_cpp_refac_test.py
+++ ==JULIEN==/src/elcic/TEMP_elc_cpp_refac_test.py
@@ -1,37 +1,28 @@
-import sys
-import re
-import os
 from elc.energy.legacy_elc_energy import get_legacy_energy
 import numpy as np
+import espressomd
 import math
 
-import io
-import re
-import copy
-import os
-import sys
-import io
-import re
-import espressomd
-import numpy as np
-from elcic.energy.legacy_contrib_framework.CUSTOM.custom import get_elcic_energy
-from elcic.energy.legacy_contrib_framework.param_lerp_plot_2d import run_lerp_plot
-from elcic.energy.legacy_contrib_framework.get_legacy_contribs import get_legacy_contribs
+
 NESSECARY_KEYS = ["lx", "ly", "lz", "gap_size", "pw_error", "prefactor"]
 OPTIONAL_KEYS = ["delta_mid_top", "delta_mid_bot"]
+
 
 def lerp(a, b, t):
     """Linear interpolation between a and b."""
     return (1 - t) * a + t * b
 
+
 def lerp_dict(start_params, end_params, t):
     """Modular interpolation of parameters."""
-    lerp_params = {key: lerp(start_params[key], end_params[key], t) for key in NESSECARY_KEYS}
-    
+    lerp_params = {
+        key: lerp(start_params[key], end_params[key], t) for key in NESSECARY_KEYS
+    }
+
     for key in OPTIONAL_KEYS:
         if key in start_params and key in end_params:
             lerp_params[key] = lerp(start_params[key], end_params[key], t)
-            
+
     lerp_params["positions"] = [
         lerp(np.array(p_start), np.array(p_end), t)
         for p_start, p_end in zip(start_params["positions"], end_params["positions"])
@@ -52,7 +43,10 @@ start_params = {
     "delta_mid_bot": -1.0,
     "charges": [+1.0, -1.0],
     "pw_error": 1e-8,
-    "positions": [np.array([6, 5, 4]), np.array([3, 2, 4])], # legacy fails for part.z <= 3    
+    "positions": [
+        np.array([6, 5, 4]),
+        np.array([3, 2, 4]),
+    ],  # legacy fails for part.z <= 3
 }
 start_params["lz"] = start_params["gap_size"] + 10
 
@@ -60,13 +54,16 @@ start_params["lz"] = start_params["gap_size"] + 10
 end_params = {
     "lx": 50.0,
     "ly": 50.0,
-    "gap_size": 15.0, # legacy runs with: 14, 15, fails with 16, 20
+    "gap_size": 15.0,  # legacy runs with: 14, 15, fails with 16, 20
     "prefactor": 1.0,
     "delta_mid_top": +1.0,
     "delta_mid_bot": +1.0,
     "charges": [+1.0, -1.0],
     "pw_error": 1e-8,
-    "positions": [np.array([6, 5, 30]), np.array([3, 2, 30])], # legacy runs for part.z = 4, 24, fails for part.z = 3, 34, 39    
+    "positions": [
+        np.array([6, 5, 30]),
+        np.array([3, 2, 30]),
+    ],  # legacy runs for part.z = 4, 24, fails for part.z = 3, 34, 39
 }
 end_params["lz"] = start_params["gap_size"] + 40
 
@@ -91,6 +88,15 @@ for t in np.linspace(0, 1, num=N):
 
 print(f"{N=}: {energies=}")
 
-"""
-N=10: energies=[-0.2765154822643574, -0.2500702524722067, -0.24313882209277052, -0.24012013430195772, -0.2380948418255439, -0.2363104533087212, -0.23452703427924, -0.23264505761174437, -0.23060694727871123, -0.2283717076533031]
-"""
+ABS_TOL = 9e-7 # elc.cpp isnt deterministic. for same params, it yields results within 9e-7
+ground_truth_energies_N10=[-0.2765154822643574, -0.2500702524722067, -0.24313750776329318, -0.24012013430195772, -0.2380948418255439, -0.2363104533087212, -0.23452703427924, -0.23264505761174437, -0.23060694727871123, -0.2283717076533031]
+
+assert len(energies) == len(ground_truth_energies_N10)
+assert all(
+    math.isclose(a, b, abs_tol=ABS_TOL) for a, b in zip(energies, ground_truth_energies_N10)
+)
+
+
+
+
+
