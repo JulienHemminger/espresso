74e64325760ac89bf76010ca42c08bdb20ca6eca
JulienHemminger <julien.hemminger@gmail.com>
2026-06-01 16:38:19 +0200

    cpp refac test

---
+++ ==JULIEN==/src/elcic/TEMP_elc_cpp_refac_test.py
@@ -0,0 +1,96 @@
+import sys
+import re
+import os
+from elc.energy.legacy_elc_energy import get_legacy_energy
+import numpy as np
+import math
+
+import io
+import re
+import copy
+import os
+import sys
+import io
+import re
+import espressomd
+import numpy as np
+from elcic.energy.legacy_contrib_framework.CUSTOM.custom import get_elcic_energy
+from elcic.energy.legacy_contrib_framework.param_lerp_plot_2d import run_lerp_plot
+from elcic.energy.legacy_contrib_framework.get_legacy_contribs import get_legacy_contribs
+NESSECARY_KEYS = ["lx", "ly", "lz", "gap_size", "pw_error", "prefactor"]
+OPTIONAL_KEYS = ["delta_mid_top", "delta_mid_bot"]
+
+def lerp(a, b, t):
+    """Linear interpolation between a and b."""
+    return (1 - t) * a + t * b
+
+def lerp_dict(start_params, end_params, t):
+    """Modular interpolation of parameters."""
+    lerp_params = {key: lerp(start_params[key], end_params[key], t) for key in NESSECARY_KEYS}
+    
+    for key in OPTIONAL_KEYS:
+        if key in start_params and key in end_params:
+            lerp_params[key] = lerp(start_params[key], end_params[key], t)
+            
+    lerp_params["positions"] = [
+        lerp(np.array(p_start), np.array(p_end), t)
+        for p_start, p_end in zip(start_params["positions"], end_params["positions"])
+    ]
+    lerp_params["charges"] = [
+        lerp(q_start, q_end, t)
+        for q_start, q_end in zip(start_params["charges"], end_params["charges"])
+    ]
+    return lerp_params
+
+
+start_params = {
+    "lx": 10.0,
+    "ly": 10.0,
+    "gap_size": 10.0,
+    "prefactor": 1.0,
+    "delta_mid_top": -1.0,
+    "delta_mid_bot": -1.0,
+    "charges": [+1.0, -1.0],
+    "pw_error": 1e-8,
+    "positions": [np.array([6, 5, 4]), np.array([3, 2, 4])], # legacy fails for part.z <= 3    
+}
+start_params["lz"] = start_params["gap_size"] + 10
+
+
+end_params = {
+    "lx": 50.0,
+    "ly": 50.0,
+    "gap_size": 15.0, # legacy runs with: 14, 15, fails with 16, 20
+    "prefactor": 1.0,
+    "delta_mid_top": +1.0,
+    "delta_mid_bot": +1.0,
+    "charges": [+1.0, -1.0],
+    "pw_error": 1e-8,
+    "positions": [np.array([6, 5, 30]), np.array([3, 2, 30])], # legacy runs for part.z = 4, 24, fails for part.z = 3, 34, 39    
+}
+end_params["lz"] = start_params["gap_size"] + 40
+
+system = espressomd.System(box_l=[50, 50, 50])
+system.time_step = 0.01
+system.cell_system.skin = (
+    0.4  # required to fix "tuning failed: number of cells 6 is smaller than minimum 8"
+)
+
+N = 10
+energies = []
+for t in np.linspace(0, 1, num=N):
+    params = lerp_dict(start_params, end_params, t)
+
+    system.electrostatics.clear()
+    system.part.clear()
+    system.box_l = [params["lx"], params["ly"], params["lz"]]
+    for i in range(len(params["charges"])):
+        system.part.add(pos=params["positions"][i], q=params["charges"][i])
+
+    energies.append(get_legacy_energy(system, params))
+
+print(f"{N=}: {energies=}")
+
+"""
+N=10: energies=[-0.2765154822643574, -0.2500702524722067, -0.24313882209277052, -0.24012013430195772, -0.2380948418255439, -0.2363104533087212, -0.23452703427924, -0.23264505761174437, -0.23060694727871123, -0.2283717076533031]
+"""
