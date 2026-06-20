0bd8e018745e83853ad564124ce8ed5b07e7a2bd
JulienHemminger <julien.hemminger@gmail.com>
2026-06-20 16:50:29 +0200

    madelung_plot

--- ==JULIEN==/test/report_plot_generation/elc/madelung_plot.py
+++ ==JULIEN==/test/report_plot_generation/elc/madelung_plot.py
@@ -42,28 +42,17 @@ def run_madelung(system, ions_list, gap_size=1, accuracy=1e-6):
         elc_energy = get_elc_energy(system, gap_size, accuracy)
         elc_energies.append(elc_energy)
 
-        print(f"{madelung_2d_ref=}, {elc_energy=}, diff={np.abs(madelung_2d_ref - elc_energy)}")
+        print(f"{ions_per_axis=}: diff={np.abs(madelung_2d_ref - elc_energy)}")
 
     return madelung_refs, elc_energies
 
-"""
-system = espressomd.System(box_l=[50, 50, 50])
-system.time_step = 0.01
-system.cell_system.skin = 0.4
-
-# TODO create plot
-# xaxis: ions_per_axis
-# yaxis: madelung_2d_ref, elc_energy
-run_madelung(system, ions_list=[8])
-
-"""
 
 # Setup
 system = espressomd.System(box_l=[50, 50, 50])
 system.time_step = 0.01
 system.cell_system.skin = 0.4 
 
-ions_range = range(3, 10) # Testing different grid sizes
+ions_range = range(2, 20+1, 2) # Testing different grid sizes
 refs, elcs = run_madelung(system, ions_range)
 
 fig, ax1 = plt.subplots(figsize=(10, 6))
@@ -72,14 +61,14 @@ fig, ax1 = plt.subplots(figsize=(10, 6))
 color = 'tab:blue'
 ax1.set_xlabel('Ions per Axis')
 ax1.set_ylabel('Energy', color=color)
-ax1.plot(ions_range, refs, 'o--', color=color, label='Analytical Reference', alpha=0.7)
+ax1.scatter(ions_range, refs, color='red', label='Analytical Reference', alpha=0.7)
 ax1.plot(ions_range, elcs, 'x-', color=color, label='ELC Energy', alpha=0.7)
 ax1.tick_params(axis='y', labelcolor=color)
 ax1.legend(loc='upper left')
 
 # Secondary Y-axis: Absolute Difference
 ax2 = ax1.twinx()
-color = 'tab:red'
+color = 'tab:cyan'
 abs_diff = np.abs(np.array(refs) - np.array(elcs))
 ax2.set_ylabel('abs(Difference)', color=color)
 ax2.plot(ions_range, abs_diff, 's:', color=color, label='Difference')
@@ -87,10 +76,7 @@ ax2.tick_params(axis='y', labelcolor=color)
 ax2.set_yscale('log')  # Often useful to see differences on a log scale
 ax2.legend(loc='upper right')
 
-plt.title('2D Madelung Energy and Absolute Difference vs Grid Size')
+plt.title('2D Madelung Energy and Absolute Difference vs Grid Size, Only for even Ion Counts, since else sytems would be non-neutral')
 plt.grid(True, which='both', axis='x', linestyle='--', alpha=0.5)
 plt.tight_layout()
 plt.show()
-
-
-# fix reference_energy for ions_per_axis odd (1, 3, 5, ...)
